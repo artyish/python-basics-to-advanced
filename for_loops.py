117336ef5188a6daae43aceb8cadf819f31bd70e
Hemank <betterartyish24@gmail.com>
2025-06-11 21:28:50 +0530

    update loops

--- for_loops.py
+++ for_loops.py
@@ -1,9 +1,17 @@
-for x in range(0,10):
-    print(x)
+# for x in range(0,10):
+    # print(x)
 
-for y in reversed(range(0,10)):
-    print(y) 
+# for y in reversed(range(0,10)):
+    # print(y) 
 
 # continue to skip an iteration
 
 # break to exit the loop
+
+import time
+
+time_sec = int(input("Input time in seconds for timer : "))
+for i in reversed(range(0,time_sec)):
+    print(i)
+    time.sleep(1)
+print("TIMES UP!!")
