269e4285479c6371eb00d91b7a6b02ee2f4abb30
Hemank <betterartyish24@gmail.com>
2025-06-11 21:08:29 +0530

    add format

---
+++ format_specifier.py
@@ -0,0 +1,13 @@
+a = 3.14159
+
+print(f"{a:.2f}") # upto two decimal places
+
+b = 2.15
+
+print(f"{b:.5f}") # adds zeros if more than decimal
+
+print(f"{b:10}") # 10 spaces to display output
+print(f"{a:10}") # 10 spaces to display output
+
+# , for commas for every thousand
+# can also use ,.2f for commas with two decimal places
