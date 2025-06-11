98fce78dcc67f0bdb32117bffe7dae88fa441b4b
Hemank <betterartyish24@gmail.com>
2025-06-11 22:21:31 +0530

    add forloops

--- for_loops.py
+++ for_loops.py
@@ -8,10 +8,28 @@
 
 # break to exit the loop
 
-import time
+# import time
 
-time_sec = int(input("Input time in seconds for timer : "))
-for i in reversed(range(0,time_sec)):
-    print(i)
-    time.sleep(1)
-print("TIMES UP!!")
+# time_sec = int(input("Input time in seconds for timer : "))
+# for i in reversed(range(0,time_sec)):
+    # print(i)
+    # time.sleep(1)
+# print("TIMES UP!!")
+
+# nested for loops
+
+j = int(input("Input the number of rows : "))
+i = int(input("Input the number of columns : "))
+character = input("Input character of your liking : ")
+
+for x in range(0 , i):
+    print()
+    for y in range(0 , j):
+        if(x == 0 or y == 0) :
+            print(character,end="")
+        elif(x == (i-1) or y == (j-1)):
+            print(character,end="")
+        else:
+            print(" ",end="")
+
+    
